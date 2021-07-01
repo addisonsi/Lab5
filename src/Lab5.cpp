10bab28614b78f3a5a7f3ceefb74b35ebfb13e5b
Addison Simon <addisonsimon@Addisons-MacBook-Air.local>
2021-07-01 11:30:10 -0500

    first commit

---
+++ src/Lab5.cpp
@@ -0,0 +1,24 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/addisonsimon/Lab5/src/Lab5.ino"
+// SYSTEM_MODE(MANUAL);
+// SYSTEM_THREAD(ENABLED);
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 5 "/Users/addisonsimon/Lab5/src/Lab5.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+ 
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+ 
+
+}
