192d3f16b2caa15a975eaa6fe134273481a09e84
Addison Simon <addisonsimon@Addisons-MacBook-Air.local>
2021-07-02 09:59:43 -0500

    commit

--- src/Lab5.ino
+++ src/Lab5.ino
@@ -1,15 +1,16 @@
-// SYSTEM_MODE(MANUAL);
-// SYSTEM_THREAD(ENABLED);
+ SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
- 
+  pinMode(A4, OUTPUT);
+  pinMode(A5, INPUT);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
- 
+  analogWrite(A4, map(analogRead(A5), 0, 1023, 255, 0));
 
 }
